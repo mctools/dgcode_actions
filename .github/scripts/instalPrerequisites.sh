93faeda629d6bf6e4668aeffb4cda7a5f683cd02
MilanKlausz <Milan.Klausz@ess.eu>
2023-05-09 20:41:20 +0200

    install emacs-nox instead of emacs

--- .github/scripts/instalPrerequisites.sh
+++ .github/scripts/instalPrerequisites.sh
@@ -5,7 +5,7 @@
 if [[ $@ == "Linux" ]]; then
   sudo apt-get update
   #TK testing: added gdb and valgrid while debugging!
-  sudo apt-get install curl g++ build-essential python3-dev cmake libxerces-c-dev libexpat1-dev gfortran libhdf5-serial-dev git python3-venv python3-pip python3-tk mesa-common-dev libglu1-mesa-dev freeglut3-dev cm-super gdb valgrind emacs
+  sudo apt-get install curl g++ build-essential python3-dev cmake libxerces-c-dev libexpat1-dev gfortran libhdf5-serial-dev git python3-venv python3-pip python3-tk mesa-common-dev libglu1-mesa-dev freeglut3-dev cm-super gdb valgrind emacs-nox
   #sudo apt install texlive texlive-latex-extra texlive-fonts-recommended dvipng #debugging
 elif [[ $@ == "macOS"  ]]; then
   brew install cmake python python-tk expat xerces-c hdf5 freetype emacs # git
