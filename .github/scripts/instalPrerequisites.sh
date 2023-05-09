85b85c99fa64b8041ed31d3b5614badb87b9f092
MilanKlausz <Milan.Klausz@ess.eu>
2023-05-09 19:07:36 +0200

    added emacs for more convenient tmate debugging

--- .github/scripts/instalPrerequisites.sh
+++ .github/scripts/instalPrerequisites.sh
@@ -5,10 +5,10 @@
 if [[ $@ == "Linux" ]]; then
   sudo apt-get update
   #TK testing: added gdb and valgrid while debugging!
-  sudo apt-get install curl g++ build-essential python3-dev cmake libxerces-c-dev libexpat1-dev gfortran libhdf5-serial-dev git python3-venv python3-pip python3-tk mesa-common-dev libglu1-mesa-dev freeglut3-dev cm-super gdb valgrind
+  sudo apt-get install curl g++ build-essential python3-dev cmake libxerces-c-dev libexpat1-dev gfortran libhdf5-serial-dev git python3-venv python3-pip python3-tk mesa-common-dev libglu1-mesa-dev freeglut3-dev cm-super gdb valgrind emacs
   #sudo apt install texlive texlive-latex-extra texlive-fonts-recommended dvipng #debugging
 elif [[ $@ == "macOS"  ]]; then
-  brew install cmake python python-tk expat xerces-c hdf5 freetype # git
+  brew install cmake python python-tk expat xerces-c hdf5 freetype emacs # git
 else
   echo "Wrong operating system"
   exit 1
